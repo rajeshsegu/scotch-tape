b1b7a10f77d14c813f5ae35c79676293d60285e9
Rajesh Segu <rajesh.segu@gmail.com>
2015-07-13 01:40:56 -0700

    test custom local asserts

---
+++ test/test-local-asserts.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var scotchTape = require('../scotch-tape');
+
+var runBasicTests = scotchTape({
+  asserts: {
+    isValidName: function customAssert(name) {
+      this.ok(name);
+      this.equal(name, 'test');
+    }
+    /* any number of local custom asserts*/
+  }
+});
+
+runBasicTests('scotch-tape tests', function test(it) {
+  it('should get called', function should(t) {
+    t.ok(true);
+    t.end();
+  });
+  it('should support custom assert', function should(t) {
+    t.ok(t.isValidName);
+    t.isValidName('test');
+    t.end();
+  });
+});
+
+var runTests = scotchTape();
+runTests('scotch-tape tests', function test(it) {
+  it('should get called', function should(t) {
+    t.ok(true);
+    t.end();
+  });
+  it('should not support custom assert', function should(t) {
+    t.notOk(t.isValidName);
+    t.end();
+  });
+});
